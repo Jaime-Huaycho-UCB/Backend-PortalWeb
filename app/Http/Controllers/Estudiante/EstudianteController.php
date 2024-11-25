677e75de4184dcd58adc853e0fd4dc7fa8ba8c3d
Jaime-MacBook <huaychojaime@gmail.com>
2024-11-24 23:13:23 -0400

    correcions sql 2

--- app/Http/Controllers/Estudiante/EstudianteController.php
+++ app/Http/Controllers/Estudiante/EstudianteController.php
@@ -21,7 +21,7 @@ class EstudianteController extends Controller{
             }else{
                 $query ="SELECT e.id, e.nombre, n.nombre as nivelAcademico, e.correo, e.tesis, e.foto ".
                 "FROM ESTUDIANTE e,NIVEL_ACADEMICO n ".
-                "WHERE e.ELiminado = 0";
+                "WHERE e.ELiminado = 0 AND n.id = e.nivelAcademico";
                 $estudiantes=DB::select($query);
             }
             
